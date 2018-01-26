294ac25e66fa764b20ea20d493ce6d133b4910d6
Ivan Sapozhnik <sapozhnik.ivan@gnail.com>
2018-01-26 21:54:12 +0100

    xcode project added

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,14 +8,19 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/vapor/vapor.git", majorVersion: 2, minor: 0),
-        .package(url: "https://github.com/vapor/vapor.git", from: "2.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Catoci",
-            dependencies: []),
+//        .package(url: "https://github.com/vapor/vapor.git", from: "2.0")
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
+
+//import PackageDescription
+//
+//let package = Package(
+//    name: "SlackBot",
+//    dependencies: [
+//        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+//    ],
+//    exclude: [
+//        "Images"
+//    ]
+//)
