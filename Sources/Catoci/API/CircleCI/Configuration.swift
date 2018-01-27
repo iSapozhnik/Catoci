b43643f7b67c66bfb2ecadd933fc615d490e9d35
Ivan Sapozhnik <sapozhnik.ivan@gnail.com>
2018-01-27 14:23:21 +0100

    CircleCI config

---
+++ Sources/Catoci/API/CircleCI/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  Catoci
+//
+//  Created by Ivan Sapozhnik on 1/27/18.
+//
+//
+
+import Foundation
+
+struct Configuration {
+    static let token: String = "25b85f27223170432a16eb48d6773e1ce6ba7b0b"
+}
